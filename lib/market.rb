de826a6f0b97d4717355bf9ba3dba16b21889bdb
Saryn Mooney <smooney15@my.whitworth.edu>
2020-06-10 10:48:28 -0600

    trying to finish up iteration 2

--- lib/market.rb
+++ lib/market.rb
@@ -1,9 +1,32 @@
+require './lib/vendor'
+
 class Market
   attr_reader :name, :vendors
 
   def initialize(name)
     @name = name
     @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    @vendors.select do |vendor|
+     vendor.inventory.include?(item.name)
+    end
+  end
+
+  def potential_revenue
+    @vendors.each do |vendor|
+      #for each item, need to mulptiply amount by price, and if more than one item, add those products together
   end
 
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -12,6 +12,90 @@ class MarketTest < Minitest::Test
     assert_instance_of Market, market
   end
 
+  def test_it_has_attributes
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", market.name
+    assert_equal [], market.vendors
+  end
+
+  def test_it_can_add_vendors
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal [vendor1, vendor2, vendor3], market.vendors
+  end
+
+  def test_it_can_return_vendor_names
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], market.vendor_names
+  end
+
+  def test_it_can_return_vendors_based_on_products
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+    assert_equal [vendor1, vendor3], market.vendors_that_sell(item1)
+  end
+
+  def test_it_can_calclulate_potential_revenue
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+  end
 end
 
 
@@ -34,66 +118,7 @@ end
 # Additionally, the Market should have a method called `vendors_that_sell` that takes an argument of an item represented as a String. It will return a list of Vendors that have that item in stock.
 #
 # Use TDD to create a `Market` class and update your `Vendor` class to responds to the following interaction pattern:
-#
-# ```ruby
-# pry(main)> require './lib/item'
-# #=> true
-#
-# pry(main)> require './lib/vendor'
-# #=> true
-#
-# pry(main)> require './lib/market'
-# #=> true
-#
-# pry(main)> market = Market.new("South Pearl Street Farmers Market")
-# #=> #<Market:0x00007fe134933e20...>
-#
-# pry(main)> market.name
-# #=> "South Pearl Street Farmers Market"
-#
-# pry(main)> market.vendors
-# #=> []
-#
-# pry(main)> vendor1 = Vendor.new("Rocky Mountain Fresh")
-# #=> #<Vendor:0x00007fe1348a1160...>
-#
-# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# pry(main)> item2 = Item.new({name: 'Tomato', price: "$0.50"})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-# pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-#
-# pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-#
-# pry(main)> vendor1.stock(item1, 35)
-#
-# pry(main)> vendor1.stock(item2, 7)
-#
-# pry(main)> vendor2 = Vendor.new("Ba-Nom-a-Nom")
-# #=> #<Vendor:0x00007fe1349bed40...>
-#
-# pry(main)> vendor2.stock(item4, 50)
-#
-# pry(main)> vendor2.stock(item3, 25)
-#
-# pry(main)> vendor3 = Vendor.new("Palisade Peach Shack")
-# #=> #<Vendor:0x00007fe134910650...>
-#
-# pry(main)> vendor3.stock(item1, 65)
-#
-# pry(main)> market.add_vendor(vendor1)
-#
-# pry(main)> market.add_vendor(vendor2)
-#
-# pry(main)> market.add_vendor(vendor3)
-#
-# pry(main)> market.vendors
-# #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
-#
+
 # pry(main)> market.vendor_names
 # #=> ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
 #
